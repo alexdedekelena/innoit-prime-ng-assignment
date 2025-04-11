18980ade5d4c9cdd4b3cacb50faee4c3fb19fa90
Alexander <alexdedekelena@gmail.com>
2025-04-11 21:33:05 +0200

    feat(router): add contact page and route to make navbar works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './core/layout/home/home.component';
+import { ContactPageComponent } from './features/contact-page/contact-page.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactPageComponent },
   { path: '**', component: HomeComponent },
 ];
