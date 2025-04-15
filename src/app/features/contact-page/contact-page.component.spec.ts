a739fc088fb9c5d142a3d87abb514d61b8e3eb69
Alexander <alexdedekelena@gmail.com>
2025-04-15 21:09:34 +0200

    test: add coverage on contact-page component

--- src/app/features/contact-page/contact-page.component.spec.ts
+++ src/app/features/contact-page/contact-page.component.spec.ts
@@ -3,9 +3,10 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ContactPageComponent } from './contact-page.component';
 import { MessageService } from 'primeng/api';
 import { ContactPageStateService } from '../../shared/services/state/contact-page-state.service';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { Component, signal } from '@angular/core';
 import { Toast } from 'primeng/toast';
+import { serialize } from 'v8';
 
 // Toast Component have 'subscribe of undefined' issue when initialized
 // Use a Mock component instead to avoid errors out of scope this component
@@ -24,7 +25,7 @@ describe('ContactPageComponent', () => {
     add: jest.fn(),
   };
   const contactPageStateServiceStub = {
-    success$: of(''),
+    success$: of('success'),
     firstName: signal(''),
     lastName: signal(''),
     email: signal(''),
@@ -35,6 +36,7 @@ describe('ContactPageComponent', () => {
     setLastName: jest.fn(),
     setEmail: jest.fn(),
     setMessage: jest.fn(),
+    sendContactMessage: jest.fn(),
   };
 
   beforeEach(waitForAsync(() => {
@@ -69,4 +71,60 @@ describe('ContactPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('onSubmit', () => {
+    it('should call contactPageStateService.sendContactMessage', () => {
+      component.onSubmit();
+      expect(contactPageStateServiceStub.sendContactMessage).toHaveBeenCalled();
+    });
+  });
+
+  describe('onFocusOutUpdateState', () => {
+    it('should call contactPageStateService.setFirstName with form control value', () => {
+      component.contactForm.controls.firstName.setValue('NEW_VALUE');
+      component.onFocusOutUpdateState('firstName');
+      expect(contactPageStateServiceStub.setFirstName).toHaveBeenCalledWith(
+        'NEW_VALUE'
+      );
+    });
+
+    it('should call contactPageStateService.setLastName with form control value', () => {
+      component.contactForm.controls.lastName.setValue('NEW_VALUE');
+      component.onFocusOutUpdateState('lastName');
+      expect(contactPageStateServiceStub.setLastName).toHaveBeenCalledWith(
+        'NEW_VALUE'
+      );
+    });
+
+    it('should call contactPageStateService.setEmail with form control value', () => {
+      component.contactForm.controls.email.setValue('NEW_VALUE');
+      component.onFocusOutUpdateState('email');
+      expect(contactPageStateServiceStub.setEmail).toHaveBeenCalledWith(
+        'NEW_VALUE'
+      );
+    });
+
+    it('should call contactPageStateService.setMessage with form control value', () => {
+      component.contactForm.controls.message.setValue('NEW_VALUE');
+      component.onFocusOutUpdateState('message');
+      expect(contactPageStateServiceStub.setMessage).toHaveBeenCalledWith(
+        'NEW_VALUE'
+      );
+    });
+  });
+
+  // TODO: Check how to trigger success$ from service
+  // describe('success$', () => {
+  //   it('should call contactForm.reset on service success$', waitForAsync(() => {
+  //     jest.spyOn(component.contactForm, 'reset');
+
+  //     contactPageStateServiceStub.success$ = new BehaviorSubject('success');
+  //     (contactPageStateServiceStub.success$ as BehaviorSubject<string>).next(
+  //       'success'
+  //     );
+  //     contactPageStateServiceStub.success$.subscribe(() =>
+  //       expect(component.contactForm.reset).toHaveBeenCalled()
+  //     );
+  //   }));
+  // });
 });
